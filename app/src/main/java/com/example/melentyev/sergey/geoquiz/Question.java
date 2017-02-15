2d872a49e67b03d86e24a59ba40e35a95de734b5
sergeyMelentyev <sergey.melentyev@gmail.com>
2017-02-15 17:10:39 +0300

    new version

---
+++ app/src/main/java/com/example/melentyev/sergey/geoquiz/Question.java
@@ -0,0 +1,17 @@
+package com.example.melentyev.sergey.geoquiz;
+
+class Question {
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public Question(int textResId, boolean answerTrue) {
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+    }
+
+    public boolean isAnswerTrue() { return mAnswerTrue; }
+    public void setAnswerTrue(boolean answerTrue) { mAnswerTrue = answerTrue; }
+    public int getTextResId() { return mTextResId; }
+    public void setTextResId(int textResId) { mTextResId = textResId; }
+
+}
---
+++ app/src/main/java/com/example/melentyev/sergey/geoquiz/QuizActivity.java
@@ -0,0 +1,89 @@
+package com.example.melentyev.sergey.geoquiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class QuizActivity extends AppCompatActivity {
+
+    private Button mTrueButton;
+    private Button mFalseButton;
+    private ImageButton mNextButton;
+    private ImageButton mPrevButton;
+
+    private TextView mQuestionTextView;
+    private int mCurrentIndex = 0;
+    private Question[] mQuestionBank = new Question[] {
+            new Question(R.string.question_australia, true),
+            new Question(R.string.question_oceans, true),
+            new Question(R.string.question_mideast, false),
+            new Question(R.string.question_africa, false),
+            new Question(R.string.question_americas, true),
+            new Question(R.string.question_asia, true)
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz);
+        mTrueButton = (Button) findViewById(R.id.true_button);
+        mFalseButton = (Button) findViewById(R.id.false_button);
+        mNextButton = (ImageButton) findViewById(R.id.next_button);
+        mPrevButton = (ImageButton) findViewById(R.id.previous_button);
+
+        mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
+
+        askNextQuestion();
+
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isQuestionCorrect(true);
+            }
+        });
+
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isQuestionCorrect(false);
+            }
+        });
+
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mCurrentIndex == mQuestionBank.length -1)
+                    mCurrentIndex = 0;
+                else ++mCurrentIndex;
+                askNextQuestion();
+            }
+        });
+
+        mPrevButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mCurrentIndex == 0)
+                    mCurrentIndex = mQuestionBank.length -1;
+                else --mCurrentIndex;
+                askNextQuestion();
+            }
+        });
+    }
+
+    private void askNextQuestion() {
+        int question = mQuestionBank[mCurrentIndex].getTextResId();
+        mQuestionTextView.setText(question);
+    }
+
+    private void isQuestionCorrect(boolean userPressedTrue) {
+        if (mQuestionBank[mCurrentIndex].isAnswerTrue() == userPressedTrue)
+            Toast.makeText(QuizActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
+        else
+            Toast.makeText(QuizActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
+    }
+}
